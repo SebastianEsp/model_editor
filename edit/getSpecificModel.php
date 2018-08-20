b20daaab311774980e52e9943dd05552c39b3964
Sebastian Esp Nielsen <sebastianesp@hotmail.com>
2018-08-20 13:10:48 +0200

    Added ability to edit model

--- edit/getSpecificModel.php
+++ edit/getSpecificModel.php
@@ -27,10 +27,7 @@
     for ($i=0; $i < $elements->length; $i++) { 
 
         if($elements[$i]->nodeValue == $title){
-            //$elements[$i]->nodeValue = "TEST";
             $root = $elements[$i]->parentNode;
-
-            var_dump($root->childNodes);
         }
     }
 
@@ -79,30 +76,4 @@
     }        
     
     echo '</table>';
-
-    //$xml->save('../xml/modelkatalog.rdf.xml');
-
-    /*$xml = simplexml_load_file("../xml/modelkatalog.rdf.xml");
-    $models = $xml->xpath('//dct:title[@xml:lang="da"]');
-
-    foreach($models as $model) {
-        if($model == $title){
-            $element = $model; 
-        }
-    }
-
-    $node = dom_import_simplexml($element);
-    $start = $node->parentNode->getLineNo();*/
-
-    $elementEnd = $root->xpath('..//dcat:distribution[last()]');
-    $nodeEnd = dom_import_simplexml($elementEnd[0]);
-    $end = $nodeEnd->getLineNo() + 1;
-
-    echo $start . ' -- ' . $end;
-
-    /*$currentElement = $element->xpath('..');
-
-    var_dump($node->nodeValue);
-    $node->nodeValue = "TEST";
-    var_dump($node->nodeValue);*/
 ?>
--- edit/save.php
+++ edit/save.php
@@ -18,14 +18,13 @@
         }
     }
 
-    try{
+    //Keep track of skips made due to comment elements
+    $skips = 0;
 
     for ($i=0; $i < $root->childNodes->length; $i++) { 
         //Checks that a tagname has been set for the node. This makes sure comments are exlucded as they do not have a tagname
+        
         if(isset($root->childNodes[$i]->tagName)){
-
-            
-
             //preg_match_all('/(\\S*):(\\S*)/', $chunks[$i][1], $matches, PREG_PATTERN_ORDER);
             
             //$replacement = new DOMDocument();
@@ -35,57 +34,35 @@
             //$newNode = $xml->importNode($replacement->documentElement, true);
 
             //Get namespaceUri for the given prefix
-            $uri = $xml->documentElement->lookupnamespaceURI($root->childNodes[$i]->prefix);
+            $pfx = $root->childNodes[$i]->prefix;
 
-            $replacement = $xml->createElementNS($uri, $chunks[$i][0], $chunks[$i][3]); //Create new node and update prefix, name and value
+            $uri = $xml->documentElement->lookupnamespaceURI($pfx);
+
+            $replacement = $xml->createElementNS($uri, $chunks[$i - $skips][0], $chunks[$i - $skips][3]); //Create new node and update prefix, name and value
 
             //Separate the preifx and name.
-            //preg_match_all('/(\\S*):(\\S*)/', $chunks[$i][1], $matches, PREG_PATTERN_ORDER);
-            
+            preg_match_all('/(\\S*):(\\S*)/', $chunks[$i - $skips][1], $matches, PREG_PATTERN_ORDER);
+
             //If node has an attribute, add attribute field.
             if(isset($matches[0][0])){
 
                 //Get namespaceUri for the given prefix
                 $uri = $xml->documentElement->lookupnamespaceURI($matches[1][0]);
 
-                $attribute = $xml->createAttributeNS($uri, $chunks[$i][1]); //Create the new attribute field
-                $attribute->value = $chunks[$i][2]; //Add value to the attribute
-    
+                $attribute = $xml->createAttributeNS($uri, $chunks[$i - $skips][1]); //Create the new attribute field
+                $attribute->value = $chunks[$i - $skips][2]; //Add value to the attribute
+
                 $replacement->appendChild($attribute); //Append attribute to the replacement element
             }
-            //$root->childNodes[$i]->parentNode->replaceChild($replacement, $root->childNodes[$i]); //Replace the old element with the new updated replacement element
+            //var_dump("original: " . $root->childNodes[$i]->tagName);
+            //var_dump("new: " .$replacement->tagName);
+            $root->childNodes[$i]->parentNode->replaceChild($replacement, $root->childNodes[$i]); //Replace the old element with the new updated replacement element
+        }else{
+            //When a comment node is encountered increment $skips
+            $skips +=1;
         }
     }
-    }catch(Exception $e){
-        echo 'Caught exception: ',  $e->getMessage(), "\n";
-        var_dump($e->getTrace());
-    }
-
-    $counter = 0;
-    //Loop through the array of elements from table and update the corresponding fields in the xml
-    /*foreach ($root->childNodes as $node) {
-        //Checks that a tagname has been set for the node. This makes sure comments are exlucded as they do not have a tagname
-        if(isset($node->tagName)){
-
-            $replacement = $xml->createElementNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', $chunks[$counter][0]); //Create new node and update name
-            $replacement->nodeValue = $chunks[$counter][3]; //Update node value
-
-            //Separate the preifx and name.
-            preg_match_all('/(\\S*):(\\S*)/', $chunks[$counter][1], $matches, PREG_PATTERN_ORDER);
-            
-            //If node has an attribute, add attribute field.
-            if(isset($matches[0][0])){
-                echo 'TEST';
-                $attribute = $xml->createAttributeNS('http://www.w3.org/1999/02/22-rdf-syntax-ns#', $chunks[$counter][1]); //Create the new attribute field
-                $attribute->value = $chunks[$counter][2]; //Add value to the attribute
-    
-                $replacement->appendChild($attribute); //Append attribute to the replacement element
-            }
-
-            $node->parentNode->replaceChild($replacement, $node); //Replace the old element with the new updated replacement element
-            $counter++;
-        }
-    }*/
 
     $xml->save('../../xml/modelkatalog.rdf.xml');
+
 ?>
